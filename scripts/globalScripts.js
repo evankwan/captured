b24c5347d0156e806b263672b5a10fc28ff1f9cd
Evan Kwan <evanyuekwan@gmail.com>
2021-05-01 10:03:56 -0400

    made hamburger menu tabable and clickable without mouse

--- scripts/globalScripts.js
+++ scripts/globalScripts.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector("#hamburger");
 
-hamburger.addEventListener("click", function () {
+hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("checked");
 })
